6623ce6f60aa39af050cb1928fda2b8c65125860
Richard Guo <richardg2025@student.cis.edu.hk>
2024-07-17 11:45:38 +0800

    working spotify search

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-// Client/src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import './App.css';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
